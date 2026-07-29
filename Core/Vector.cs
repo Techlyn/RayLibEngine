f985703c21ec9e9553ffc26dd67aa77ee9423292
Techlyn <nic_nacs_didleydot@hotmail.co.uk>
2026-07-29 17:58:43 +0100

    overloaded operators for Vector.cs

--- Core/Vector.cs
+++ Core/Vector.cs
@@ -26,6 +26,30 @@ namespace RayLibEngine.Core
         public float x { get { return m_x; } set { m_x = value; } }
         public float y { get { return m_y; } set { m_y = value; } }
 
+        public float GetMagnitude()
+        {
+            double result = (double)(m_x * m_x + m_y * m_y);
+            return (float)Math.Sqrt(result);
+        }
+
+
+        public Vector Normalize()
+        {
+            Vector v = new Vector(m_x, m_y);
+            float length = GetMagnitude();
+            if (length > 0)
+            {
+                v.x = m_x / length;
+                v.y = m_y / length;
+            }
+            return v;
+        }
+
+        public Vector Scale(float s)
+        {
+            return new Vector(m_x * s, m_y * s);
+        }
+
 
         public static Vector operator +(Vector a, Vector b) => new Vector(a.x + b.x, a.y + b.y);
         public static Vector operator -(Vector a, Vector b) => new Vector(a.x - b.x, a.y - b.y);
@@ -35,9 +59,27 @@ namespace RayLibEngine.Core
         public static bool operator >(Vector a, Vector b) => (a.x > b.x) && (a.y > b.y);
         public static bool operator <=(Vector a, Vector b) => (a.x <= b.x) && (a.y <= b.y);
         public static bool operator >=(Vector a, Vector b) => (a.x >= b.x) && (a.y >= b.y);
-        public static bool operator ==(Vector a, Vector b) => (Equals(a.x, b.x)) && (Equals(a.y, b.y));
-        public static bool operator !=(Vector a, Vector b) => !(a == b);
+        public static bool operator ==(Vector a, Vector b)
+        {
+            if(a is null)
+            {
+                return b is null;
+            }
+            return a.Equals(b);
+        }
 
+        public static bool operator !=(Vector a, Vector b)
+        {
+            if(a is null)
+            {
+                return b is not null;
+            }
+            return !a.Equals(b);
+        }
 
+        public bool Equals(Vector? other) => other is not null && other == this;
+        public override bool Equals(object? obj) => Equals(obj as Vector);
+
+        
     }
 }
