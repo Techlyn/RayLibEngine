f55aad66a2bee1288b579a119bed6a2282ecc191
Techlyn <nic_nacs_didleydot@hotmail.co.uk>
2026-07-29 17:09:16 +0100

    Vector.cs operator changes.

---
+++ Core/Vector.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RayLibEngine.Core
+{
+    public class Vector
+    {
+        private float m_x;
+        private float m_y;
+
+
+        public Vector(float init_x, float init_y)
+        {
+            m_x = init_x;
+            m_y = init_y;
+        }
+
+        public Vector()
+        {
+            m_x = 0;
+            m_y = 0;
+        }
+
+
+        public float x { get { return m_x; } set { m_x = value; } }
+        public float y { get { return m_y; } set { m_y = value; } }
+
+
+        public static Vector operator +(Vector a, Vector b) => new Vector(a.x + b.x, a.y + b.y);
+        public static Vector operator -(Vector a, Vector b) => new Vector(a.x - b.x, a.y - b.y);
+        public static Vector operator *(Vector a, Vector b) => new Vector(a.x * b.x, a.y * b.y);
+        public static Vector operator /(Vector a, Vector b) => new Vector(a.x / b.x, a.y / b.y);
+        public static bool operator <(Vector a, Vector b) => (a.x < b.x) && (a.y < b.y);
+        public static bool operator >(Vector a, Vector b) => (a.x > b.x) && (a.y > b.y);
+        public static bool operator <=(Vector a, Vector b) => (a.x <= b.x) && (a.y <= b.y);
+        public static bool operator >=(Vector a, Vector b) => (a.x >= b.x) && (a.y >= b.y);
+        public static bool operator ==(Vector a, Vector b) => (Equals(a.x, b.x)) && (Equals(a.y, b.y));
+        public static bool operator !=(Vector a, Vector b) => !(a == b);
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,19 +9,35 @@ namespace RayLibEngine;
 
 internal static class Program
 {
-    
+
 
 
     static void Main(string[] args)
-    {   
+    {
         Logger.StartUp();
         Logger.SetGlobalLevel(LogLevel.LOG_DEBUG);
 
-        Engine.Run();
+        //Engine.Run();
+        Vector test = new() { x = 0, y = 0 };
+        Vector test2 = new() { x = 6, y = 20 };
+
+        test.x = 5;
+        test.y = 21;
+
+        WriteLog($"Vector1 components: ({test.x}, {test.y})");
+        WriteLog($"Vector2 components: ({test2.x}, {test2.y})");
+        WriteLog($"Adding Vectors together and making that the new Vector2");
+
+        if (test < test2) ;
+
+        WriteLog($"Vector2 component ({test2.x}, {test2.y})");
+
+        
+
 
         return;
 
-       
+
     }
 
    
